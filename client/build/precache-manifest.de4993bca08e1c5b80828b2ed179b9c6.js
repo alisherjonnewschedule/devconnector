1b8d0087939371760015fae8a9150b5a2aca55f0
unknown <egamberdiyevalisherjon@gmail.com>
2020-11-10 16:12:36 +0500

    initial commit

---
+++ client/build/precache-manifest.de4993bca08e1c5b80828b2ed179b9c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "878362654a8102318093d13eda6ceced",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec4bb0a45e876e461c6d",
+    "url": "/static/css/main.c6d8f597.chunk.css"
+  },
+  {
+    "revision": "d99e5f6fe77e7d84f2e7",
+    "url": "/static/js/2.ac675a64.chunk.js"
+  },
+  {
+    "revision": "ec4bb0a45e876e461c6d",
+    "url": "/static/js/main.754a83db.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  },
+  {
+    "revision": "528795c979660217494b0bed465f9a9e",
+    "url": "/static/media/showcase.528795c9.jpg"
+  }
+]);
